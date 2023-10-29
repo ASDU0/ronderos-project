7e405186bdafafc367fb82a3435bab6ceebe0ce9
roswell <roswell_pando@outlook.es>
2023-10-29 10:54:38 -0500

    formulario para terminar un crimen finalizado

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+
+import styles from '../styles/registroCrimen.module.css';
+
+function Header() {
+  return (
+
+    <header className={styles.header}>
+      <h1>DATOS DEL CRIMEN</h1>
+    </header>
+  );
+}
+export default Header;
--- src/app/nuevo-crimen/page.tsx
+++ src/app/nuevo-crimen/page.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+'use client';
 
-const page = () => {
+import Header from '../components/Header';
+import Formulario from '../components/Formulario';
+import styles from '../styles/page.module.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function registroCrimen() {
   return (
-    <>
-      <Navbar />
-      <div>Registra un nuevo crimen</div>
-    </>
-  )
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <div>
+          <Header />
+          <br />
+          <Formulario />
+        </div>
+      </div>
+    </main>
+  );
 }
-
-export default page
