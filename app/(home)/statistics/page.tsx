1d760905f6faadf7e6be426ff60dcc930beb6d82
edumm <192666@unsaac.edu.pe>
2023-11-22 20:55:24 -0500

    adding separator and statistics page is completed

--- app/(home)/statistics/page.tsx
+++ app/(home)/statistics/page.tsx
@@ -2,6 +2,7 @@ import Map from "@/components/maps";
 import {Button} from "@/components/ui/button";
 import {DatePicker} from "@/components/date-picker";
 import StatisticsCard from "@/components/statistics/statistics-card";
+import FooterStatistics from "@/components/statistics/footer-statistics-";
 const StatisticsPage = () => {
     return (
         <div>
@@ -9,6 +10,7 @@ const StatisticsPage = () => {
             <div className="bg-gray-800 flex flex-col gap-y-4 py-5">
                 <StatisticsCard />
             </div>
+            <FooterStatistics />
         </div>
     );
 };
--- components/statistics/statistics-card.tsx
+++ components/statistics/statistics-card.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {useState} from "react";
+import React, {useState} from "react";
 import {Button} from "@/components/ui/button";
 import {DatePicker} from "@/components/date-picker";
 import {ScrollArea, ScrollBar} from "@/components/ui/scroll-area";
@@ -7,6 +7,9 @@ import PersonDressIcon from "@/components/person-dress-icon";
 import PersonIcon from "@/components/person-icon";
 import {CircleDollarSign, Moon, Sun} from "lucide-react";
 import PoliceIcon from "@/components/police-icon";
+import {Separator} from "@/components/ui/separator";
+import {PiePercentageChart} from "@/components/charts/pie-percentage-chart";
+import {crimeList} from "@/components/charts/utils";
 
 const StatisticsCard = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
@@ -105,8 +108,160 @@ const StatisticsCard = () => {
                     </div>
                 </div>
                 <ScrollBar orientation="horizontal" />
-            <ScrollBar orientation="horizontal" />
-        </ScrollArea>
+            </ScrollArea>
+            
+            <div className="px-5">
+                <p className="text-2xl text-gray-200 font-semibold p-5">
+                    TIPOS DE CRIMENES MAS COMETIDOS
+                </p>
+                <Separator />
+
+                <div className="flex mt-6 px-4 py-6">
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="px-5">
+                <p className="text-2xl text-gray-200 font-semibold p-5">
+                    OBJETOS MAS ROBADOS
+                </p>
+                <Separator />
+
+                <div className="flex mt-6 px-4 py-6">
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                    <div className="w-1/6 flex flex-col items-center justify-center">
+                        <PiePercentageChart crimeList={crimeList} />
+                        <p className="text-lg text-gray-200 font-mono ">
+                            2.339
+                        </p>
+                        <p className="text-lg text-gray-200 font-mono">
+                            Marcas
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="px-5">
+                <p className="text-2xl text-gray-200 font-semibold p-5">
+                    RANKING DE BARRIOS
+                </p>
+               <div className="p-10">
+                   <div className="rounded-lg border-2 border-gray-200 flex items-center justify-around py-6 px-2">
+                       <p className="text-xl text-gray-300">
+                           1. Barrio Uno
+                       </p>
+                       <p className="text-xl text-gray-300">
+                           2. Barrio Dos
+                       </p>
+                       <p className="text-xl text-gray-300">
+                           3. Barrio Tres
+                       </p>
+                       <p className="text-xl text-gray-300">
+                           4. Barrio Cuatro
+                       </p>
+                       <p className="text-xl text-gray-300">
+                           5. Barrio Cinco
+                       </p>
+                   </div>
+               </div>
+            </div>
         </>
 
     );
