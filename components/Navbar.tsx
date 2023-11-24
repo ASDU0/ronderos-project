0e904bb93cd88b9719df2e39b7d2221cc0002627
edumm <192666@unsaac.edu.pe>
2023-11-24 00:29:02 -0500

    refactor header and fix errors in console

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
+import {Search} from "lucide-react";
 
 const Navbar = () => {
 
@@ -32,9 +33,7 @@ const Navbar = () => {
           <div className="base-mobile lg:flex-grow">
             <div className="relative inline-block ml-6 mt-2">
                   <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                    </svg>
+                    <Search className="text-gray-400" />
                     <span className="sr-only">Search icon</span>
                   </div>
                   <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-3xl focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar Ciudad"/>
@@ -68,9 +67,7 @@ const Navbar = () => {
             </Link>
             <div className="relative inline-block ml-6 mt-2">
                   <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                    </svg>
+                    <Search className="text-gray-400" />
                     <span className="sr-only">Search icon</span>
                   </div>
                   <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-3xl focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar Ciudad"/>
